e112f380f84be6001a3363c797b2f6cc2e740cb7
glinkinzhenya <glinkinzhenya@gmail.com>
2023-04-11 16:59:32 +0300

    finish2.0

--- src/pages/AddForm/AddForm.jsx
+++ src/pages/AddForm/AddForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Button } from '@mui/material';
 import { InputText } from '../../components/Forms/InputText';
@@ -7,21 +7,53 @@ import './AddForm.css';
 
 export default function AddForm() {
   const { control, handleSubmit, getValues } = useForm();
-  const putForm = async () => {
-    console.log(getValues());
-    await fetch('https://64148167e8fe5a3f3a087de9.mockapi.io/api/v1/classes', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: getValues().name,
-        image: getValues().image,
-        fullInfo: getValues().fullInfo,
-        gallery: [getValues().image_1, getValues().image_2, getValues().image_3, getValues().image_4],
-      }),
+
+  const [result, setResult] = useState({});
+
+  const test = () => {
+    console.log(getValues().fullInfo);
+    const days = getValues().fullInfo.split('; ');
+    console.log(days);
+    const obj = {};
+
+    days.forEach((day) => {
+      const [name, time, value] = day.split(', ');
+      obj[name] = obj[name] || {};
+      obj[name][time] = value;
     });
+
+    setResult({ days: obj });
+    const finish = { days: obj };
+    console.log(result);
+    console.log({ days: obj });
+    console.log(finish);
   };
+
+  // const putForm = async () => {
+  //   // const days = {};
+  //   // console.log(getValues().fullInfo);
+  //   // const data = getValues().fullInfo;
+  //   // const newData = data.split(' ');
+  //   // console.log(newData);
+  //   // newData.forEach((item) => {
+  //   //   console.log(item);
+  //   //   days[item] = item;
+  //   // });
+  //   // console.log(days);
+
+  //   await fetch('https://64148167e8fe5a3f3a087de9.mockapi.io/api/v1/classes', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       name: getValues().name,
+  //       image: getValues().image,
+  //       fullInfo: getValues().fullInfo,
+  //       gallery: [getValues().image_1, getValues().image_2, getValues().image_3, getValues().image_4],
+  //     }),
+  //   });
+  // };
 
   return (
     <div className='add-form-wrapper'>
@@ -33,21 +65,18 @@ export default function AddForm() {
           rules={addFormRules.login}
         />
         <InputText
-          style={{ backgroundColor: 'black' }}
           control={control}
           name='image'
           label='Логотип'
           rules={addFormRules.image}
-          id="myFileInput"
         />
         <InputText
-          style={{ backgroundColor: 'black' }}
           control={control}
           name='fullInfo'
           label='Опис тренування'
           rules={addFormRules.description}
         />
-        <InputText
+        {/* <InputText
           style={{ backgroundColor: 'black' }}
           control={control}
           name='image_1'
@@ -74,122 +103,10 @@ export default function AddForm() {
           name='image_4'
           label='Зображення 4'
           rules={addFormRules.image}
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='Понеділок'
-          label='Понеділок'
-          rules=''
-          type='checkbox'
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='пн_10:30'
-          label='10:30'
-          rules=''
-          type='checkbox'
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='пн_10:30_seats'
-          label='Кількість місць'
-          rules=''
-          type='number'
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='пн_11:30'
-          label='11:30'
-          rules=''
-          type='checkbox'
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='пн_11:30_seats'
-          label='Кількість місць'
-          rules=''
-          type='number'
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='пн_12:30'
-          label='12:30'
-          rules=''
-          type='checkbox'
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='пн_12:30_seats'
-          label='Кількість місць'
-          rules=''
-          type='number'
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='Середа'
-          label='Середа'
-          rules=''
-          type='checkbox'
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='ср_10:30'
-          label='10:30'
-          rules=''
-          type='checkbox'
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='ср_10:30_seats'
-          label='Кількість місць'
-          rules=''
-          type='number'
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='ср_11:30'
-          label='11:30'
-          rules=''
-          type='checkbox'
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='ср_11:30_seats'
-          label='Кількість місць'
-          rules=''
-          type='number'
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='ср_12:30'
-          label='12:30'
-          rules=''
-          type='checkbox'
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='ср_12:30_seats'
-          label='Кількість місць'
-          rules=''
-          type='number'
-        />
+        /> */}
         <Button
           size='large'
-          onClick={handleSubmit(putForm)}
+          onClick={handleSubmit(test)}
         >Додати
         </Button>
       </div>
