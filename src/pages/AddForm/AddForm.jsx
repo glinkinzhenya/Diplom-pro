ddb9261d1792b0f7022e9f82d158f5fb6b551492
glinkinzhenya <glinkinzhenya@gmail.com>
2023-04-11 13:16:49 +0300

    days

--- src/pages/AddForm/AddForm.jsx
+++ src/pages/AddForm/AddForm.jsx
@@ -7,10 +7,21 @@ import './AddForm.css';
 
 export default function AddForm() {
   const { control, handleSubmit, getValues } = useForm();
-
-  function pushServer() {
+  const putForm = async () => {
     console.log(getValues());
-  }
+    await fetch('https://64148167e8fe5a3f3a087de9.mockapi.io/api/v1/classes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: getValues().name,
+        image: getValues().image,
+        fullInfo: getValues().fullInfo,
+        gallery: [getValues().image_1, getValues().image_2, getValues().image_3, getValues().image_4],
+      }),
+    });
+  };
 
   return (
     <div className='add-form-wrapper'>
@@ -32,7 +43,7 @@ export default function AddForm() {
         <InputText
           style={{ backgroundColor: 'black' }}
           control={control}
-          name='description'
+          name='fullInfo'
           label='Опис тренування'
           rules={addFormRules.description}
         />
@@ -64,9 +75,121 @@ export default function AddForm() {
           label='Зображення 4'
           rules={addFormRules.image}
         />
+        <InputText
+          style={{ backgroundColor: 'black' }}
+          control={control}
+          name='Понеділок'
+          label='Понеділок'
+          rules=''
+          type='checkbox'
+        />
+        <InputText
+          style={{ backgroundColor: 'black' }}
+          control={control}
+          name='пн_10:30'
+          label='10:30'
+          rules=''
+          type='checkbox'
+        />
+        <InputText
+          style={{ backgroundColor: 'black' }}
+          control={control}
+          name='пн_10:30_seats'
+          label='Кількість місць'
+          rules=''
+          type='number'
+        />
+        <InputText
+          style={{ backgroundColor: 'black' }}
+          control={control}
+          name='пн_11:30'
+          label='11:30'
+          rules=''
+          type='checkbox'
+        />
+        <InputText
+          style={{ backgroundColor: 'black' }}
+          control={control}
+          name='пн_11:30_seats'
+          label='Кількість місць'
+          rules=''
+          type='number'
+        />
+        <InputText
+          style={{ backgroundColor: 'black' }}
+          control={control}
+          name='пн_12:30'
+          label='12:30'
+          rules=''
+          type='checkbox'
+        />
+        <InputText
+          style={{ backgroundColor: 'black' }}
+          control={control}
+          name='пн_12:30_seats'
+          label='Кількість місць'
+          rules=''
+          type='number'
+        />
+        <InputText
+          style={{ backgroundColor: 'black' }}
+          control={control}
+          name='Середа'
+          label='Середа'
+          rules=''
+          type='checkbox'
+        />
+        <InputText
+          style={{ backgroundColor: 'black' }}
+          control={control}
+          name='ср_10:30'
+          label='10:30'
+          rules=''
+          type='checkbox'
+        />
+        <InputText
+          style={{ backgroundColor: 'black' }}
+          control={control}
+          name='ср_10:30_seats'
+          label='Кількість місць'
+          rules=''
+          type='number'
+        />
+        <InputText
+          style={{ backgroundColor: 'black' }}
+          control={control}
+          name='ср_11:30'
+          label='11:30'
+          rules=''
+          type='checkbox'
+        />
+        <InputText
+          style={{ backgroundColor: 'black' }}
+          control={control}
+          name='ср_11:30_seats'
+          label='Кількість місць'
+          rules=''
+          type='number'
+        />
+        <InputText
+          style={{ backgroundColor: 'black' }}
+          control={control}
+          name='ср_12:30'
+          label='12:30'
+          rules=''
+          type='checkbox'
+        />
+        <InputText
+          style={{ backgroundColor: 'black' }}
+          control={control}
+          name='ср_12:30_seats'
+          label='Кількість місць'
+          rules=''
+          type='number'
+        />
         <Button
           size='large'
-          onClick={handleSubmit(pushServer)}
+          onClick={handleSubmit(putForm)}
         >Додати
         </Button>
       </div>
