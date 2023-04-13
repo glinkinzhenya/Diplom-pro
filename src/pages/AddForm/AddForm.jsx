7e0fd3b338c602f755fcf0dd4f262e8c7226dad4
glinkinzhenya <glinkinzhenya@gmail.com>
2023-04-13 19:26:30 +0300

    form 1 and form 2

--- src/pages/AddForm/AddForm.jsx
+++ src/pages/AddForm/AddForm.jsx
@@ -1,115 +1,151 @@
+// import React, { useState } from 'react';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { Button } from '@mui/material';
+import { Button, Grid, TextField } from '@mui/material';
 import { InputText } from '../../components/Forms/InputText';
 import { addFormRules } from '../../constans/rules';
 import './AddForm.css';
 
 export default function AddForm() {
-  const { control, handleSubmit, getValues } = useForm();
-
+  const [day, setDay] = useState('');
+  const [time, setTime] = useState('');
+  const [value, setValue] = useState('');
   const [result, setResult] = useState({});
 
-  const test = () => {
-    console.log(getValues().fullInfo);
-    const days = getValues().fullInfo.split('; ');
-    console.log(days);
-    const obj = {};
+  let data2 = '';
 
-    days.forEach((day) => {
-      const [name, time, value] = day.split(', ');
-      obj[name] = obj[name] || {};
-      obj[name][time] = value;
-    });
+  const handleSubmitTwo = (event) => {
+    event.preventDefault();
 
-    setResult({ days: obj });
-    const finish = { days: obj };
-    console.log(result);
-    console.log({ days: obj });
-    console.log(finish);
+    const obj = { ...result };
+
+    if (!obj[day]) {
+      obj[day] = {};
+    }
+
+    obj[day][time] = value;
+    setResult(obj);
+    setDay('');
+    setTime('');
+    setValue('');
   };
 
-  // const putForm = async () => {
-  //   // const days = {};
-  //   // console.log(getValues().fullInfo);
-  //   // const data = getValues().fullInfo;
-  //   // const newData = data.split(' ');
-  //   // console.log(newData);
-  //   // newData.forEach((item) => {
-  //   //   console.log(item);
-  //   //   days[item] = item;
-  //   // });
-  //   // console.log(days);
+  console.log(result);
+  data2 = result;
 
-  //   await fetch('https://64148167e8fe5a3f3a087de9.mockapi.io/api/v1/classes', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({
-  //       name: getValues().name,
-  //       image: getValues().image,
-  //       fullInfo: getValues().fullInfo,
-  //       gallery: [getValues().image_1, getValues().image_2, getValues().image_3, getValues().image_4],
-  //     }),
-  //   });
-  // };
+  const { control, handleSubmit, getValues } = useForm();
+
+  const putForm = async () => {
+    await fetch('https://64148167e8fe5a3f3a087de9.mockapi.io/api/v1/classes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: getValues().name,
+        image: getValues().image,
+        fullInfo: getValues().fullInfo,
+        gallery: [getValues().image_1, getValues().image_2, getValues().image_3, getValues().image_4],
+        data2,
+      }),
+    });
+  };
 
   return (
-    <div className='add-form-wrapper'>
-      <div className='add-form'>
-        <InputText
-          control={control}
-          name='name'
-          label='Назва тренування'
-          rules={addFormRules.login}
-        />
-        <InputText
-          control={control}
-          name='image'
-          label='Логотип'
-          rules={addFormRules.image}
-        />
-        <InputText
-          control={control}
-          name='fullInfo'
-          label='Опис тренування'
-          rules={addFormRules.description}
-        />
-        {/* <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='image_1'
-          label='Зображення 1'
-          rules={addFormRules.image}
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='image_2'
-          label='Зображення 2'
-          rules={addFormRules.image}
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='image_3'
-          label='Зображення 3'
-          rules={addFormRules.image}
-        />
-        <InputText
-          style={{ backgroundColor: 'black' }}
-          control={control}
-          name='image_4'
-          label='Зображення 4'
-          rules={addFormRules.image}
-        /> */}
-        <Button
-          size='large'
-          onClick={handleSubmit(test)}
-        >Додати
-        </Button>
+    <>
+      <div className='add-form-wrapper'>
+        <form className='add-form' onSubmit={handleSubmitTwo}>
+          <InputText
+            control={control}
+            name='name'
+            label='Назва тренування'
+            rules={addFormRules.login}
+          />
+          <InputText
+            control={control}
+            name='image'
+            label='Логотип'
+            rules={addFormRules.image}
+          />
+          <InputText
+            control={control}
+            name='fullInfo'
+            label='Опис тренування'
+            rules={addFormRules.description}
+          />
+          <InputText
+            style={{ backgroundColor: 'black' }}
+            control={control}
+            name='image_1'
+            label='Зображення 1'
+            rules={addFormRules.image}
+          />
+          <InputText
+            style={{ backgroundColor: 'black' }}
+            control={control}
+            name='image_2'
+            label='Зображення 2'
+            rules={addFormRules.image}
+          />
+          <InputText
+            style={{ backgroundColor: 'black' }}
+            control={control}
+            name='image_3'
+            label='Зображення 3'
+            rules={addFormRules.image}
+          />
+          <InputText
+            style={{ backgroundColor: 'black' }}
+            control={control}
+            name='image_4'
+            label='Зображення 4'
+            rules={addFormRules.image}
+          />
+
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                label='Day'
+                variant='outlined'
+                fullWidth
+                value={day}
+                onChange={(e) => setDay(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label='Time'
+                variant='outlined'
+                fullWidth
+                value={time}
+                onChange={(e) => setTime(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label='Value'
+                variant='outlined'
+                fullWidth
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant='contained' type='submit' fullWidth>
+                Submit
+              </Button>
+            </Grid>
+            <Grid item xs={12}>
+              <pre>{JSON.stringify(result, null, 2)}</pre>
+            </Grid>
+          </Grid>
+          <Button
+            size='large'
+            onClick={handleSubmit(putForm)}
+          >Додати
+          </Button>
+        </form>
       </div>
-    </div>
+    </>
   );
 }
